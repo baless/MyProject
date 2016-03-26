ab35e4be12fca84c0a7d3616aab9426230a73f57
baless <dmswpwh@gmail.com>
2016-03-27 02:04:01 +0900

    asset file
    asset file

---
+++ Asset/Script/DestroyParticle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticle : MonoBehaviour {
+
+    ParticleSystem GetParticle;
+
+	// Use this for initialization
+	void Start () {
+        GetParticle = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetParticle.isPlaying == false)
+            Destroy(gameObject);
+	}
+}
