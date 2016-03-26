a93e292cdafa595328358874d6765d2859b77f5d
baless <dmswpwh@gmail.com>
2016-03-27 01:56:54 +0900

    Added files via upload

---
+++ asset/TitleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour {
+
+    public Text highScoreLabel;
+
+    public void Start()
+    {
+        Screen.SetResolution(1280,800,true);
+        highScoreLabel.text = "High Score : " + PlayerPrefs.GetInt("HighScore") + "m";
+    }
+
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void OnStartButtonClicked()
+    {
+        Application.LoadLevel("Main");
+    }
+}
