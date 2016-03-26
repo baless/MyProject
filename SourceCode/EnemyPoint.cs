8ba0b3c865f35c3cb5e08786719014744970cad2
baless <dmswpwh@gmail.com>
2016-03-27 02:02:00 +0900

    unity source code
    unity source code

---
+++ SourceCode/EnemyPoint.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPoint : MonoBehaviour {
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        //프리팹을 같은 포지션에 생성
+        GameObject go = (GameObject)Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+        //같은 삭제되도록 생성한 적 오브젝트를 자식으로 설정
+        go.transform.SetParent(transform, false);
+	}
+    //스테이지 에디트 기즈모를 표시
+    void OnDrawGizmos()
+    {
+        //기즈모의 하단이 지면과 같은 높이가 되도록 오프셋설정
+        Vector3 offset = new Vector3(0, 0.5f, 0);
+
+        //동그라미를 표시
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position + offset, 0.5f);
+
+        //프리팹 이름의 아이콘표시
+        if (prefab != null)
+            Gizmos.DrawIcon(transform.position + offset, prefab.name, true);
+    }
+}
---
+++ SourceCode/GameController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public CharactorController character;
+    public Text Score;
+    public Text Life;
+    public Text DeadScore;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+	// Update is called once per frame
+	public void Update () {
+
+        if(Application.platform == RuntimePlatform.Android) { 
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.LoadLevel("Title");
+            }
+        }
+
+        int score = CalcScore();
+        Score.text = "Score : " + score + "m";
+
+        int deadscore = CalcScore();
+        DeadScore.text = "最終スコア : " + deadscore + "m";
+
+        int life = CalcLife();
+        Life.text = "Life Point : " + life;
+
+
+        if (PlayerPrefs.GetInt("HighScore") < score)
+            PlayerPrefs.SetInt("HighScore", score);
+
+        if (character.Life() <= 0)
+        {
+            enabled = false;
+            GameObject.Find("ToMain").GetComponent<UnityEngine.UI.Image>().enabled = true;
+            GameObject.Find("ToReStart").GetComponent<UnityEngine.UI.Image>().enabled = true;
+            GameObject.Find("DeadScore").GetComponent<UnityEngine.UI.Text>().enabled = true;
+        }
+    }
+
+    int CalcScore()
+    {
+        return (int)character.transform.position.z;
+    }
+
+    int CalcLife()
+    {
+        return character.life;
+    }
+
+    public void ReturnToTitle()
+    {
+        Application.LoadLevel("Title");
+    }
+
+    public void ReStart()
+    {
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+}
